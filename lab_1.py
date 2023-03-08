128286ac15519207de9959aedbcee3fbfc7e8b99
TomYDoma <qwerty135261@gmail.com>
2023-03-08 17:40:25 +0300

    End lab 4 ///refresh

--- lab_1.py
+++ lab_1.py
@@ -1,14 +1,14 @@
 import cmath
 from tkinter import *
-from tkinter import ttk
+from tkinter.ttk import Treeview
+
+import numpy as np
 from PIL import ImageTk, Image
 from math import *
 root = Tk()
 root.title("Лабораторные работы по ИСРПО Улитиной К.В.")
 root.geometry("500x400")
 root.configure(bg="beige")
-
-
 
 
 #создание окна лабы 1
@@ -71,8 +71,6 @@ def createWindowLab_1():
 
     fr_lab1.pack()
 
-
-
 def createWindowLab_2():
     def remove_text():
         label_one.config(text="")
@@ -129,8 +127,6 @@ def createWindowLab_2():
     label_one.pack(padx=6, pady=6)
 
     fr_lab2.pack()
-
-
 
 def createWindowLab_3():
     def remove_text():
@@ -206,6 +202,137 @@ def createWindowLab_3():
     fr_lab3.pack()
 
 
+def createWindowLab_4():
+    def remove_text():
+        label_one.config(text="")
+
+
+    def show_answer():
+        remove_text()
+        try:
+            data = []
+            a = int(entry_a.get())
+            b = int(entry_b.get())  # получаем введенный текст, entry- поле
+            n = int(entry_n.get())
+
+            k = (b - a) / n
+            m = 0
+            for i in np.arange(b, a - 0.5, -k):
+                m += 1
+                if i <= -6:
+                    x = i
+                    y = 1
+                    ab = (m, x, y)
+                    data.append(ab)
+
+                elif -6 < i <= -4:
+                    x = i
+                    y = - 0.5 * x - 2
+                    ab = (m, x, y)
+                    data.append(ab)
+
+                elif -4 < i <= 0:
+                    x = i
+                    y = sqrt(4 - pow(x + 2, 2))
+                    ab = (m, x, y)
+                    data.append(ab)
+
+                elif 0 < i <= 2:
+                    x = i
+                    y = - sqrt(1 - pow(x - 1, 2))
+                    ab = (m, x, y)
+                    data.append(ab)
+
+                elif i > 2:
+
+                    x = i
+                    y = - x + 2
+                    ab = (m, x, y)
+                    data.append(ab)
+
+            print(data)
+
+            def clear_all():
+                for item in tree.get_children():
+                    tree.delete(item)
+
+            # определяем столбцы
+            columns = ("n", "x", "y")
+
+
+            tree = Treeview(fr_lab4, columns=columns, show="headings")
+            tree.pack(fill=BOTH, expand=1)
+
+
+            # определяем заголовки
+            tree.heading("n", text="N")
+            tree.heading("x", text="X")
+            tree.heading("y", text="Y")
+
+            tree.column("#1", width=70)
+            tree.column("#2", width=70)
+            tree.column("#3", width=70)
+
+            # добавляем данные
+            for i in data:
+                tree.insert("", END, values=i)
+
+
+        except:
+
+            label_one["text"] = "Некорректный ввод"
+
+    lab_4 = Toplevel()
+    lab_4.title("Лабораторная работа 4")
+    lab_4.geometry("500x500")
+    lab_4.configure(bg="beige")
+    lab_4.resizable(width=False, height=False)  # нельзя менять размер ячеек
+
+    # создание фрейма
+    fr_lab4 = Frame(lab_4, background="beige")
+    quest = Label(lab_4,
+                  text="Лабораторная работа 4 \n "
+                       "Разработать приложение для вывода значений функции y = f(x) в точках, \n "
+                       " полученных делением отрезка [a,b] на N равных частей ",
+                  bg="beige")
+    quest.pack()
+
+    # вывод изображения
+    img = Image.open("assets/Lab2_graf.png")
+    pil_image = img.resize((200, 100))
+    fr_lab4.image = ImageTk.PhotoImage(pil_image)
+    image_sprite = Label(fr_lab4, image=fr_lab4.image, bg="beige")
+    image_sprite.pack(anchor=NW)
+
+    # кнопка,текстовое полe и вывод результата
+    label_n = Label(fr_lab4, text="Введите N", bg="beige")
+    label_n.pack()
+
+    entry_n = Entry(fr_lab4, bg="beige")
+    entry_n.pack(padx=6, pady=6)
+
+    label_a = Label(fr_lab4, text="Введите a", bg="beige")
+    label_a.pack()
+
+    entry_a = Entry(fr_lab4, bg="beige")
+    entry_a.pack(padx=6, pady=6)
+
+    label_b = Label(fr_lab4, text="Введите b", bg="beige")
+    label_b.pack()
+
+    entry_b = Entry(fr_lab4, bg="beige")
+    entry_b.pack(padx=6, pady=6)
+
+    btn = Button(fr_lab4, text="Запустить", command=show_answer, bg="beige")
+    btn.pack(padx=6, pady=6)
+
+    label_one = Label(fr_lab4, bg="beige")
+    label_one.pack()
+
+
+
+    fr_lab4.pack()
+
 
 lab_1 = Button(root, text="lab_1", command=createWindowLab_1, bg="#d6f3c2", activebackground="beige", width=20)
 lab_1.pack(anchor=NW, pady=5, padx=20)
@@ -213,7 +340,7 @@ lab_2 = Button(text="lab_2", command=createWindowLab_2, bg="#d6f3c2", activeback
 lab_2.pack(anchor=NW, pady=5, padx=20)
 lab_3 = Button(text="lab_3", command=createWindowLab_3, bg="#d6f3c2", activebackground="beige", width=20)
 lab_3.pack(anchor=NW, pady=5, padx=20)
-lab_4 = Button(text="lab_4", bg="#d6f3c2", activebackground="beige", width=20)
+lab_4 = Button(text="lab_4", command=createWindowLab_4, bg="#d6f3c2", activebackground="beige", width=20)
 lab_4.pack(anchor=NW, pady=5, padx=20)
 lab_5 = Button(text="lab_5", bg="#d6f3c2", activebackground="beige", width=20)
 lab_5.pack(anchor=NW, pady=5, padx=20)
